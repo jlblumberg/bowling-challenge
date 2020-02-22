ea9c566196853d4fe0387d063f25cfb6e0d27e49
Josh Blumberg <jlblumberg@gmail.com>
2020-02-22 14:18:57 +0000

    Second test for game of all ones passes

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -8,6 +8,13 @@ describe('Bowling outcome', function () {
     for (i = 0; i < 20; i++) {
       game.roll(0);
     }
-    expect(game.score()).toEqual(0)
+    expect(game.score()).toEqual(0);
+  });
+
+  it('is 20 if all ones rolled', function() {
+    for (i = 0; i < 20; i++) {
+      game.roll(1);
+    }
+    expect(game.score()).toEqual(20);
   });
 });
